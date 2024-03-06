c52bfff1f45dea515605900fc1d369db80847862
devfabien <ish001fabi@gmail.com>
2024-03-06 18:21:15 +0200

    feat: creat a members array

---
+++ members.js
@@ -0,0 +1,21 @@
+const members = [
+  {
+    id: 1,
+    name: "dev Fabien",
+    email: "dev@fabien",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Karera Olivier",
+    email: "karera@olivier",
+    status: "inactive",
+  },
+  {
+    id: 3,
+    name: "algor Fernard",
+    email: "algor@fernard",
+    status: "inactive",
+  },
+];
+module.exports = members;
