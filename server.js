c439ddd73f2a897f29ef353a97697629e8c961ad
devfabien <ish001fabi@gmail.com>
2024-03-06 18:21:26 +0200

    feat: create the server

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const moment = require("moment");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// to send a file instead of a text
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+// middleware
+
+// app.use((req, res, next) => {
+//   console.log(
+//     `${req.protocol}://${req.get("host")}${
+//       req.originalUrl
+//     } :${moment().format()}`
+//   );
+//   next();
+// });
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/members", require("./routes/api/members"));
+
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+});
